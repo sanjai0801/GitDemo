00d7cf523b0646caa07d721dc770606e526190db
sanjai0801 <sanjaistrikes@gmail.com>
2025-05-01 19:24:09 +0530

    i m a tester

---
+++ src/org/test/SampleJava.java
@@ -0,0 +1,16 @@
+package org.test;
+
+public class SampleJava {
+
+	public static void main(String[] args) {
+		sampleMethod();
+
+	}
+
+	public static void sampleMethod() {
+
+		System.out.println("GIT PRACTICE");
+
+	}
+
+}
